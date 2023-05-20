1b154c3f70c84a3de38b770c138ab5dc05a583e8
Murilo Torres <129902680+MuriloT2@users.noreply.github.com>
2023-05-20 14:33:38 +0000

    aula2

--- turma-A/aula2.java
+++ turma-A/aula2.java
@@ -6,18 +6,82 @@ public class aula2 {
         
         Scanner tec = new Scanner(System.in);
 
+        // --- Exercicio 1 ---
+
+        // System.out.println("Insira um valor: ");
+        // double valor = tec.nextDouble();
+
+        // if (valor % 2 == 0) {
+
+        //     System.out.println("É par!");
+
+        // } else {
+
+        //     System.out.println("É impar!");
+
+        // }
+
+        // --- Exercicio 2 ---
+
+        // int cont = 1;
+        // int soma = 0;
+
+        // while (cont <= 100) {
+
+        //     soma += cont;
+        //     cont++;
+
+        //     System.out.println("O resultado é: " + soma);
+
+        // }
+
+        // --- Exercicio 3 ---
+
+        // int senha = 0;
+
+        // while (senha != 2208) {
+
+        //     System.out.println("Insira a senha: ");
+        //     senha = tec.nextInt();
+
+        //     if (senha != 2208) {
+        //         System.out.println("Senha inválida!");
+        //     } else {
+        //         System.out.println("Acesso permitido!");
+        //     }
+
+        // }
+
+        // --- Exercicio 4 ---
+
+        // System.out.println("Insira o valor da tabuada: ");
+        // int valorT = tec.nextInt();
+
+        // System.out.println("Tabuada do " + valorT + ":");
+
+        // for (int cont = 1; cont <= 10; cont++) {
+
+        //     int mult = valorT * cont;
+
+        //     System.out.println(valorT + " X " + cont + " = " + mult);
+
+        // }
+
+        // --- Exercicio 5 ---
+
         System.out.println("Insira um valor: ");
-        double valor = tec.nextDouble();
+        int n = tec.nextInt();
 
-        if (valor % 2 == 0) {
+        System.out.println("Números impares até " + n + ":");
 
-            System.out.println("É par!");
+        for (int cont = 0; cont < n; cont++) {
 
-        } else {
+            if (cont % 2 != 0) 
+            System.out.println(cont);
 
-            System.out.println("É impar!");
-            
         }
+
+        tec.close();
         
     }
     
