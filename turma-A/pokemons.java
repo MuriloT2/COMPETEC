ae5fda0858cd0be047b40bdd139101d8e42018a6
Murilo Torres <129902680+MuriloT2@users.noreply.github.com>
2023-08-05 14:32:14 +0000

    05_08

---
+++ turma-A/pokemons.java
@@ -0,0 +1,43 @@
+ import java.util.Scanner; 
+ 
+ public class pokemons {
+
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Insira o tamanho do arranjo: ");
+        int tamanho = tec.nextInt();
+
+    String pokemons[] = new String [tamanho];
+
+    for (int cont = 0; cont < tamanho; cont++) {
+
+        String pokemon = tec.nextLine();
+        pokemons[cont] = pokemon;
+
+    }
+
+    String pokemons2[] = new String [tamanho];
+
+    for (int cont = 0; cont < tamanho; cont++) {
+
+        pokemons2[cont] = pokemons[cont];
+
+    }
+
+    for (int cont = 0; cont < tamanho; cont++) {
+        
+        if (pokemons[cont].compareToIgnoreCase(pokemons2[cont+1]) > 0) {
+
+            String pokemonAntes = pokemons2[cont+1];
+
+            pokemons[cont] = pokemonAntes;
+
+        }
+
+}
+
+    }
+
+}
---
+++ turma-A/vacina.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class vacina {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Insira a quantidade de valores: ");
+        int quant = tec.nextInt();
+
+        int valores[] = new int[quant];
+
+        for (int i = 0; i < quant; i++) {
+
+            int valor = tec.nextInt();
+            valores[i] = valor;
+
+        }
+
+        int procura = tec.nextInt();
+
+        int cont = 0;
+
+        for (int i = 0; i < quant; i++) {
+
+            if (valores[i] == procura) {
+                cont++;
+            }
+
+        }
+
+        if (cont >= 2) {
+
+            System.out.println("Achei o danado!");
+
+        } else if (cont == 1) {
+
+            System.out.println("Ainda não é o suficiente!");
+
+        } else {
+
+            System.out.println("Sumiu!");
+            
+        }
+
+
+    }
+    
+}
